50bc16abba918affe86acc37d7f7b8e0d62badba
Priyansh-09 <priyanshsingh1@gmail.com>
2021-06-13 14:49:54 +0530

    Modified Header and added About Section

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/*jshint esversion: 6 */
+let themeSwitch = document.querySelector('.toggle-icon');
+let body = document.querySelector('body');
+themeSwitch.onclick = function(){
+    themeSwitch.classList.toggle('active');
+    body.classList.toggle('dark');
+};
